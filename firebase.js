2178b07b25e9726cd36dee62b16d5c8ec842ba45
adrianwarth8-create <adrianwarth8@gmail.com>
2026-06-22 15:17:47 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGFMk28nkFgO58YNKircKq8WKhV9So51A",
+  authDomain: "sistemamedicamentos.firebaseapp.com",
+  projectId: "sistemamedicamentos",
+  storageBucket: "sistemamedicamentos.firebasestorage.app",
+  messagingSenderId: "1098170580891",
+  appId: "1:1098170580891:web:f53f622d1acb997ca72b3e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
+window.getDocs = getDocs;
+window.deleteDoc = deleteDoc;
+window.doc = doc;
+
+console.log("🔥 Firebase conectado");
