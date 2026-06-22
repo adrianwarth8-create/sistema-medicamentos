667731cdeaec669862fcadc6d8cacc0ccdc3ba61
adrianwarth8-create <adrianwarth8@gmail.com>
2026-06-22 15:49:59 -0300

    Create protecao.js

---
+++ protecao.js
@@ -0,0 +1,25 @@
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+
+import {
+auth
+} from "./firebase.js";
+
+
+
+onAuthStateChanged(auth, (usuario)=>{
+
+
+if(!usuario){
+
+
+window.location =
+"login.html";
+
+
+}
+
+
+});
