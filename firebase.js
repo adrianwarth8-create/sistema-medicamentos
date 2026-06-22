fa709735d6f7831ab19c6602d956fd7d48950d68
adrianwarth8-create <adrianwarth8@gmail.com>
2026-06-22 15:44:32 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,72 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { 
+    initializeApp 
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  deleteDoc,
-  doc
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    doc
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAGFMk28nkFgO58YNKircKq8WKhV9So51A",
-  authDomain: "sistemamedicamentos.firebaseapp.com",
-  projectId: "sistemamedicamentos",
-  storageBucket: "sistemamedicamentos.firebasestorage.app",
-  messagingSenderId: "1098170580891",
-  appId: "1:1098170580891:web:f53f622d1acb997ca72b3e"
+
+    apiKey: "AIzaSyAGFMk28nkFgO58YNKircKq8WKhV9So51A",
+
+    authDomain: "sistemamedicamentos.firebaseapp.com",
+
+    projectId: "sistemamedicamentos",
+
+    storageBucket: "sistemamedicamentos.firebasestorage.app",
+
+    messagingSenderId: "1098170580891",
+
+    appId: "1:1098170580891:web:f53f622d1acb997ca72b3e"
+
 };
 
+
+
 const app = initializeApp(firebaseConfig);
+
+
+
 const db = getFirestore(app);
 
+
+
+const auth = getAuth(app);
+
+
+
+// Firestore
+
 window.db = db;
+
 window.collection = collection;
+
 window.addDoc = addDoc;
+
 window.getDocs = getDocs;
+
 window.deleteDoc = deleteDoc;
+
 window.doc = doc;
 
-console.log("🔥 Firebase conectado");
+
+// Login
+
+window.auth = auth;
+
+
+console.log("🔥 Firebase conectado com Login");
